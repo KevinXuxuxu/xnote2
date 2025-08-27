dd5dcff720aa9d15103423dd471ada3c22e015c2
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2025-08-26 19:23:43 -0700

    add build to release script

--- release.sh
+++ release.sh
@@ -1,2 +1,3 @@
+docker run --rm -v `pwd`:/rust_tmp -v $HOME/.cargo/registry:/usr/local/cargo/registry -w /rust_tmp rust_dev cargo build --release && \
 docker build . -t cr.local.fzxu.me:5000/xnote:0.5 && \
 docker push cr.local.fzxu.me:5000/xnote:0.5
